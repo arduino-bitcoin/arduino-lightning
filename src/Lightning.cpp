3cbe57b1e12ecb3b3ddff8c23748cdeb2f9b66e4
Stepan Snigirev <snigirev.stepan@gmail.com>
2018-09-26 16:28:26 +0200

    generate invoice

--- src/Lightning.cpp
+++ src/Lightning.cpp
@@ -1,12 +1,42 @@
 #include "Lightning.h"
 #include <Conversion.h>
+#include <Bitcoin.h>
+#include <Hash.h>
 
-#define MAX_REQUEST_SIZE 1000
+#define MAX_INVOICE_SIZE 400
 
 LightningInvoice::LightningInvoice(){};
+
+LightningInvoice::LightningInvoice(
+                    const char description[], const uint8_t preimage[32], uint32_t time,
+                    uint32_t value, char mult, bool use_testnet){
+    testnet = use_testnet;
+    amount = value;
+    multiplier = mult;
+
+    // timestamp
+    buffer = (uint8_t *) calloc( 7, sizeof(uint8_t));
+    bufLen = 7;
+
+    for(int i=0; i<8; i++){
+        buffer[6-i] = (time % 32);
+        time = (time / 32);
+    }
+
+    // hash
+    uint8_t hash[32];
+    sha256(preimage, 32, hash);
+    addField(1, hash, sizeof(hash));
+
+    // description. 
+    // TODO: description hash if len > 639
+    size_t l = strlen(description);
+    addField(13, (uint8_t *)description, l);
+}
+
 LightningInvoice::LightningInvoice(const char * invoice){
     size_t l = strlen(invoice);
-    if(l > MAX_REQUEST_SIZE){
+    if(l > MAX_INVOICE_SIZE){
         return;
     }
     size_t offset = 0;
@@ -16,15 +46,19 @@ LightningInvoice::LightningInvoice(const char * invoice){
     }
 
     size_t len = l - offset;
-    size_t data_len;
+    size_t dataLen;
     char * hrp = (char *) calloc(len-6, sizeof(char));
     uint8_t * data = (uint8_t *) calloc(len-8, sizeof(uint8_t));
-    if(!bech32_decode(hrp, data, &data_len, invoice+offset)){
+    if(!bech32_decode(hrp, data, &dataLen, invoice+offset)){
+        free(data);
+        free(hrp);
         return; // fail in decoding
     }
 
     // is it lightning invoice?
     if(memcmp(hrp, "ln", 2) != 0){
+        free(data);
+        free(hrp);
         return;
     }
     // testnet or mainnet? regtest is not implemented yet
@@ -33,6 +67,8 @@ LightningInvoice::LightningInvoice(const char * invoice){
     }else if(memcmp(hrp+2, "tb", 2) == 0){
         testnet = true;
     }else{
+        free(data);
+        free(hrp);
         return;
     }
 
@@ -44,16 +80,113 @@ LightningInvoice::LightningInvoice(const char * invoice){
             multiplier = m;
         }
     }
-    
-    buffer = (uint8_t *) calloc( data_len, sizeof(uint8_t));
-    memcpy(buffer, data, data_len);
-    bufLen = data_len;
+    // TODO: move signature from data
+    buffer = (uint8_t *) calloc( dataLen, sizeof(uint8_t));
+    memcpy(buffer, data, dataLen);
+    bufLen = dataLen;
 
     free(hrp);
     free(data);
 }
+// LightningInvoice::LightningInvoice(const LightningInvoice &other){
+//     if(other.bufLen > 0){
+//         bufLen = other.bufLen;
+//         buffer = (uint8_t *) calloc( bufLen, sizeof(uint8_t));
+//         memcpy(buffer, other.buffer, bufLen);
+//     }
+//     amount = other.amount;
+//     multiplier = other.multiplier;
+//     testnet = other.testnet;
+//     sig = other.sig;
+// }
 LightningInvoice::~LightningInvoice(){
-    free(buffer);
+    if(bufLen>0){
+        free(buffer);
+        bufLen = 0;
+    }
+}
+int LightningInvoice::addField(uint8_t code, const uint8_t * data, uint16_t dataLen){
+    uint8_t * arr;
+    arr = (uint8_t *)calloc(dataLen*2, sizeof(uint8_t));
+    if(arr == NULL){
+        return 0;
+    }
+    size_t arrLen = 0;
+    convert_bits(arr, &arrLen, 5, data, dataLen, 8, 1);
+
+    buffer = (uint8_t *) realloc( buffer, (bufLen + arrLen + 3) * sizeof(uint8_t));
+    if(buffer == NULL){
+        return 0;
+    }
+    buffer[bufLen] = code;
+    buffer[bufLen+1] = (arrLen >> 5);
+    buffer[bufLen+2] = (arrLen % 32);
+    memcpy(buffer+bufLen+3, arr, arrLen);
+    bufLen += arrLen + 3;
+    free(arr);
+    return arrLen+3;
+}
+int LightningInvoice::addRawData(const uint8_t * data, size_t dataLen){
+    buffer = (uint8_t *) realloc( buffer, (bufLen + dataLen) * sizeof(uint8_t));
+    memcpy(buffer+bufLen, data, dataLen);
+    bufLen += dataLen;
+    return dataLen;
+}
+int LightningInvoice::setExpiry(uint32_t expiry){
+    uint8_t arr[7];
+    uint8_t len = 0;
+    while(expiry > 0){
+        len++;
+        arr[7-len] = (expiry % 32);
+        expiry = expiry/32;
+    }
+    buffer = (uint8_t *) realloc( buffer, (bufLen + len + 3) * sizeof(uint8_t));
+    buffer[bufLen] = 6;
+    buffer[bufLen+1] = (len >> 5);
+    buffer[bufLen+2] = (len % 32);
+    memcpy(buffer+bufLen+3, arr+7-len, len);
+    bufLen += len + 3;
+    return len + 3;
+}
+int LightningInvoice::hmr(char * arr, size_t arrSize) const{
+    char hrp[20] = "ln";
+    if(testnet){
+        memcpy(hrp+2,"tb",2);
+    }else{
+        memcpy(hrp+2,"bc",2);
+    }
+    if(amount > 0){
+        sprintf(hrp+4,"%ld", amount);
+        if(multiplier != ' '){
+            hrp[strlen(hrp)] = multiplier;
+        }
+    }
+    size_t len = strlen(hrp);
+    if(arrSize < len){
+        return 0;
+    }
+    memset(arr, 0, arrSize);
+    memcpy(arr, hrp, len);
+    return len;
+}
+int LightningInvoice::hash(uint8_t hashArr[32]){
+    char hrp[20] = { 0 };
+    size_t len = hmr(hrp, sizeof(hrp)-1);
+    SHA256 h;
+    h.begin();
+    h.write((uint8_t *)hrp, len);
+    uint8_t data[300] = { 0 };
+    size_t dataLen = 0;
+    convert_bits(data, &dataLen, 8, buffer, bufLen, 5, 1);
+    h.write(data, dataLen);
+    h.end(hashArr);
+    return 32;
+}
+Signature LightningInvoice::sign(const PrivateKey pk){
+    uint8_t h[32];
+    hash(h);
+    sig = pk.sign(h);
+    return sig;
 }
 uint32_t LightningInvoice::timestamp() const{
     uint32_t ts;
@@ -99,23 +232,64 @@ String LightningInvoice::description() const{
     }
     return "";
 }
-size_t LightningInvoice::printTo(Print &p) const{
-    p.print("lightning:");
-    char hrp[20] = "ln";
-    if(testnet){
-        memcpy(hrp+2,"tb",2);
-    }else{
-        memcpy(hrp+2,"bc",2);
-    }
-    if(amount > 0){
-        sprintf(hrp+4,"%ld", amount);
-        if(multiplier != ' '){
-            hrp[strlen(hrp)] = multiplier;
-        }
-    }
-    char * output = (char *) calloc(strlen(hrp)+bufLen+8+1, sizeof(char));
-    bech32_encode(output, hrp, buffer, bufLen);
 
+size_t LightningInvoice::printTo(Print &p) const{
+    // p.print("lightning:");
+    char hrp[20] = { 0 };
+    hmr(hrp, sizeof(hrp));
+    uint8_t * data = (uint8_t *) calloc(bufLen + 120, sizeof(uint8_t));
+    memcpy(data, buffer, bufLen);
+    size_t len = bufLen;
+    uint8_t sig_bin[65];
+    sig.bin(sig_bin);
+    sig_bin[64] = sig.index;
+    size_t sig_len = 0;
+    convert_bits(data+len, &sig_len, 5, sig_bin, sizeof(sig_bin), 8, 1);
+    len += sig_len;
+    char * output = (char *) calloc(strlen(hrp)+len+20, sizeof(char));
+    bech32_encode(output, hrp, data, len);
     p.print(output);
-    return strlen(hrp)+bufLen+8+11;
+    size_t l = strlen(output);
+    free(data);
+    free(output);
+    return l; // just strlen would be better
 }
+
+size_t LightningInvoice::toCharArray(char * arr, size_t arrSize) const{
+    char hrp[20] = { 0 };
+    hmr(hrp, sizeof(hrp));
+    uint8_t * data = (uint8_t *) calloc(bufLen + 120, sizeof(uint8_t));
+    memcpy(data, buffer, bufLen);
+    size_t len = bufLen;
+    uint8_t sig_bin[65];
+    sig.bin(sig_bin);
+    sig_bin[64] = sig.index;
+    size_t sig_len = 0;
+    convert_bits(data+len, &sig_len, 5, sig_bin, sizeof(sig_bin), 8, 1);
+    len += sig_len;
+    size_t l = strlen(hrp)+len+20;
+    if(l > arrSize){
+        free(data);
+        return 0;
+    }
+    memset(arr, 0, arrSize);
+    bech32_encode(arr, hrp, data, len);
+    free(data);
+    return l;
+}
+
+// LightningInvoice &LightningInvoice::operator=(LightningInvoice const &other){ 
+//     if(bufLen > 0){
+//         free(buffer);
+//     }
+//     if(other.bufLen > 0){
+//         bufLen = other.bufLen;
+//         buffer = (uint8_t *) calloc( bufLen, sizeof(uint8_t));
+//         memcpy(buffer, other.buffer, bufLen);
+//     }
+//     amount = other.amount;
+//     multiplier = other.multiplier;
+//     testnet = other.testnet;
+//     sig = other.sig;
+//     return *this; 
+// };
--- src/Lightning.h
+++ src/Lightning.h
@@ -2,6 +2,7 @@
 #define __LIGHTNING_H__BDDNDVJ300
 
 #include <Arduino.h>
+#include <Bitcoin.h>
 
 class LightningInvoice : public Printable{
 private:
@@ -10,13 +11,26 @@ private:
 public:
 	LightningInvoice();
 	LightningInvoice(const char * invoice);
+	LightningInvoice(const char * description, const uint8_t preimage[32], uint32_t time,
+					uint32_t value = 0, char mult = ' ', 
+					bool use_testnet = false);
+	// LightningInvoice(const LightningInvoice &other);
 	~LightningInvoice();
 	uint32_t amount = 0;
 	char multiplier = ' ';
 	bool testnet = false;
+	Signature sig;
+	int hash(uint8_t hashArr[32]);
+	Signature sign(const PrivateKey pk);
 	uint32_t timestamp() const;
 	String description() const;
+	int addField(uint8_t code, const uint8_t * data, uint16_t dataLen);
+	int setExpiry(uint32_t expiry);
+	int addRawData(const uint8_t * data, size_t dataLen);
+	int hmr(char * arr, size_t arrSize) const;
 	size_t printTo(Print &p) const;
+	size_t toCharArray(char * arr, size_t arrSize) const;
+    // LightningInvoice &operator=(LightningInvoice const &other);
 };
 
 #endif // __LIGHTNING_H__BDDNDVJ300
